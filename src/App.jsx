5fcb8398a28e28f8e37062d4b80562825243cbff
Sayain-Aditya <as4316664@gmail.com>
2024-05-05 20:01:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+
+
+`
+
+function App() {
+
+  return (
+    <>
+     <button>
+      This is a
+     </button>
+    </>
+  )
+}
+
+export default App
